c644fecd087068514de48073259124725d5ae6b2
Mohamed AKOUZ <makouz@norsys.fr>
2021-09-09 16:14:39 +0100

    feat(dev): removing old dev sql insertions to be updated

--- src/main/resources/db/dev/dev-data.sql
+++ src/main/resources/db/dev/dev-data.sql
@@ -9,20 +9,22 @@ from course;
 
 delete from home_cover_image;
 delete from home_cover;
+delete from training_path;
 INSERT INTO home_cover VALUES (1, null, null, 'CAROUSEL');
 INSERT INTO home_cover_image VALUES (1, 'http://localhost:3900/s/L75CkXeFPFMpE2S/preview', 1);
 INSERT INTO home_cover_image VALUES (2, 'http://localhost:3900/s/2FmTNGBmDtTf2qo/preview', 1);
 INSERT INTO home_cover_image VALUES (3, 'http://localhost:3900/s/PQYo8ffT5SE2FG9/preview', 1);
 
-insert into training_path (training_path_title, image_url, training_path_description, capacity, training_path_time,
-                           pre_request, training_path_status, training_path_language)
-values ('parcours 1', 'imageUrl 1', 'parcours 1', 'test', 15, 'test', false, 'FR');
-insert into training_path (training_path_title, image_url, training_path_description, capacity, training_path_time,
-                           pre_request, training_path_status, training_path_language)
-values ('parcours 2', 'imageUrl 2', 'parcours 1', 'test', 15, 'test', false, 'FR');
-insert into training_path (training_path_title, image_url, training_path_description, capacity, training_path_time,
-                           pre_request, training_path_status, training_path_language)
-values ('parcours 3', 'imageUrl 3', 'parcours 1', 'test', 15, 'test', false, 'EN');
+-- TODO: UPDATE DEV DATA
+--insert into training_path (image_url, training_path_description, capacity, training_path_time,
+--                           pre_request, training_path_status, training_path_language)
+--values ('parcours 1', 'imageUrl 1', 'parcours 1', 'test', 15, 'test', false, 'FR');
+--insert into training_path (training_path_title, image_url, training_path_description, capacity, training_path_time,
+--                           pre_request, training_path_status, training_path_language)
+--values ('parcours 2', 'imageUrl 2', 'parcours 1', 'test', 15, 'test', false, 'FR');
+--insert into training_path (training_path_title, image_url, training_path_description, capacity, training_path_time,
+--                           pre_request, training_path_status, training_path_language)
+--values ('parcours 3', 'imageUrl 3', 'parcours 1', 'test', 15, 'test', false, 'EN');
 
 -- insert into training_path (training_path_title, image_url, full_image_path, training_path_description, capacity,
 --                            training_path_time,
