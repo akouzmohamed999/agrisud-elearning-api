ac0aa0792acf412fa4aee1787195321a008e6b52
Mohamed AKOUZ <makouz@norsys.fr>
2021-09-12 13:37:02 +0100

    feat(cloud): adding https replace for int

--- src/main/java/org/agrisud/elearningAPI/clouddao/HomeCoverImageCloudDao.java
+++ src/main/java/org/agrisud/elearningAPI/clouddao/HomeCoverImageCloudDao.java
@@ -5,6 +5,7 @@ import org.aarboard.nextcloud.api.filesharing.Share;
 import org.aarboard.nextcloud.api.filesharing.SharePermissions;
 import org.aarboard.nextcloud.api.filesharing.ShareType;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 
 import java.io.File;
@@ -15,6 +16,12 @@ public class HomeCoverImageCloudDao {
     @Autowired
     private NextcloudConnector connector;
 
+    @Value("${cloud.server.name}")
+    String serverName;
+
+    @Value("${cloud.server.download-url}")
+    String downloadUrl;
+
     public String uploadHomeCoverImage(File file, String path) {
         connector.uploadFile(file, path);
         return getHomeCoverImage(path);
@@ -23,6 +30,6 @@ public class HomeCoverImageCloudDao {
     private String getHomeCoverImage(String path) {
         SharePermissions permissions = new SharePermissions(SharePermissions.SingleRight.READ);
         Share share = connector.doShare(path, ShareType.PUBLIC_LINK, null, false, null, permissions);
-        return share.getUrl() + "/preview";
+        return share.getUrl().replace(serverName.replace("http", "https"), downloadUrl) + "/preview";
     }
 }
