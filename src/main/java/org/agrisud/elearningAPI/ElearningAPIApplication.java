b7614d3535a9b263e04d2b1dce40ac46ec2591b7
Mohamed AKOUZ <makouz@norsys.fr>
2021-11-09 17:44:29 +0100

    feat(buiild): fixing build

--- src/main/java/org/agrisud/elearningAPI/ElearningAPIApplication.java
+++ src/main/java/org/agrisud/elearningAPI/ElearningAPIApplication.java
@@ -5,36 +5,33 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import javax.annotation.PostConstruct;
-
 @SpringBootApplication
 public class ElearningAPIApplication {
 
-	@Autowired
-	NextcloudConnector connector;
 
-	@PostConstruct
-	public void initCloudFolders() {
-		if(!connector.folderExists("/Elearning")){
-			connector.createFolder("/Elearning");
-		}
-
-		if(!connector.folderExists("/Elearning/Course")){
-			connector.createFolder("/Elearning/Course");
-		}
-
-		if(!connector.folderExists("/Elearning/Course/PDF")){
-			connector.createFolder("/Elearning/Course/PDF");
-		}
-
-		if(!connector.folderExists("/Elearning/TrainingPath")){
-			connector.createFolder("/Elearning/TrainingPath");
-		}
-
-		if(!connector.folderExists("/Elearning/TrainingPath/Pictures")){
-			connector.createFolder("/Elearning/TrainingPath/Pictures");
-		}
-	}
+//	@PostConstruct
+//	@Profile("!test")
+//	public void initCloudFolders() {
+//		if(!connector.folderExists("/Elearning")){
+//			connector.createFolder("/Elearning");
+//		}
+//
+//		if(!connector.folderExists("/Elearning/Course")){
+//			connector.createFolder("/Elearning/Course");
+//		}
+//
+//		if(!connector.folderExists("/Elearning/Course/PDF")){
+//			connector.createFolder("/Elearning/Course/PDF");
+//		}
+//
+//		if(!connector.folderExists("/Elearning/TrainingPath")){
+//			connector.createFolder("/Elearning/TrainingPath");
+//		}
+//
+//		if(!connector.folderExists("/Elearning/TrainingPath/Pictures")){
+//			connector.createFolder("/Elearning/TrainingPath/Pictures");
+//		}
+//	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ElearningAPIApplication.class, args);
